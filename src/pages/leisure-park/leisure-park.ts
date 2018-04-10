fd7d272001ec93a6d3c510f0cdedd8c21f80ad6d
Cai <yaming-cai@MERCER.COM>
2018-04-10 17:26:23 +0800

    #f# fix leisure park merge issue

--- src/pages/leisure-park/leisure-park.ts
+++ src/pages/leisure-park/leisure-park.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams , ModalController } from 'ionic-angular';
 import { ILeisurePark } from '../../model/leisurePark';
 import { IUser } from '../../model/user';
 import { RestServiceProvider } from '../../providers/rest-service/rest-service';
-import { AutoCompleteServiceProvider } from '../../providers/autocomplete-service/autocomplete-service';
-
+ 
 import {ICommunity} from '../../model/community';
+
+import { SelectCommunityModalPage } from '../select-community-modal/select-community-modal';
 /**
  * Generated class for the LeisureParkPage page.
  *
@@ -24,17 +25,11 @@ export class LeisureParkPage {
   leisurePark: ILeisurePark;
   currentUser: IUser;
   showCommunity: boolean;
-  searchText: string;
-  coms : any ;
-
-
-  searchQuery: string = '';
-  items: string[];
-  hideList : boolean  ;
+ 
 
   constructor(public navCtrl: NavController,  
     public service: RestServiceProvider,
-    public autoService: AutoCompleteServiceProvider) {
+    public modalCtrl: ModalController) {
     this.leisurePark = {
       _id: '',
       id: '',
@@ -53,63 +48,13 @@ export class LeisureParkPage {
     console.log('ionViewDidLoad LeisureParkPage');
     this.currentUser = JSON.parse(localStorage.getItem('user'));
     if (this.currentUser && !this.currentUser.community_ID) {
- 
-      this.showCommunity = true;
-      
-    
-       
-      //this.initializeItems();
+      this.presentModal();
     }
   }
 
-  searchTextChagne(ev: any)
-  {
-    this.hideList = false;
-    this.autoService.getResults(ev.target.value).then( x => {
-       this.coms = x;
-       this.coms.forEach(element => {
-         JSON.stringify(element);
-       });
-       
-      // console.dir(x);
-    });
-    
+  presentModal() {
+    let modal = this.modalCtrl.create(SelectCommunityModalPage);
+    modal.present();
   }
-    
-  // initializeItems() {
-  //   this.items = [
-  //     'Amsterdam',
-  //     'Bogota',
-  //     'Boot'
-  //   ];
-  // }
-
-  
-  // addItem(item:any){
-  //   this.hideList = true;
-  //   //console.dir(item);
-  //   this.searchQuery =  item.toString();
-  // }
-
-  addItem(item:any){
-    this.hideList = true;
-    console.dir(item);
-    this.searchQuery =  item.name;
-  }
-
-  // getItems(ev: any) {
-  //   // Reset items back to all of the items
-  //   this.initializeItems();
-
-  //   // set val to the value of the searchbar
-  //   let val = ev.target.value;
-  //   this.hideList = false;
-  //   // if the value is an empty string don't filter the items
-  //   if (val && val.trim() != '') {
-  //     this.items = this.items.filter((item) => {
-  //       return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-  //     })
-  //   } 
-  // }
 
 }
