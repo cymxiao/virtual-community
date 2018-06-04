b8e5f8bd0e9e1710216cf8a96af267f6ed9d8bd6
caiyaming <caiyaming@me.com>
2018-06-04 23:08:56 +0800

    #deploy# enable prod mode

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,12 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
-import { ENV } from '@app/env';
+//import { ENV } from '@app/env';
 
 
-console.log('ENV.mode is: ' + ENV.mode);
-if (ENV.mode === 'Production') {
+// console.log('ENV.mode is: ' + ENV.mode);
+// if (ENV.mode === 'Production') {
     enableProdMode();
-  }
+  //}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
